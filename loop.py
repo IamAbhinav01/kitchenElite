b0dde9c264d889d123371dc739bf994e225ed060
Abhinav <AbhinavSunil@hotmail.com>
2026-01-22 15:50:46 +0530

    home page section almost done

--- loop.py
+++ loop.py
@@ -5,8 +5,15 @@
 #     print(f"{ask} x {i} = {result}"
 
 
-for i in range(1, 6):
-    for j in range(1, 6-i):
-        print(j, end=' ')
-    print()
+# for i in range(1, 6):
+#     for j in range(1, 6-i):
+#         print(j, end=' ')
+#     print()
         
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n*factorial(n-1)
+
+
+print(factorial(5))
